29c1272e4f3b91e47d765e78930c42308dee9d7c
unknown <usuario@PUERTA18.CORP.AR>
2022-05-13 12:41:45 -0300

    eliminando el directorio dist

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Persona_1 = require("./clases/Persona");
-let fede = new Persona_1.Persona(1984, "Federico");
-console.log(fede.cualEsTuNombre());
-console.log(fede.cualEsTuGeneracion());
-console.log(fede.calculartuEdad());
